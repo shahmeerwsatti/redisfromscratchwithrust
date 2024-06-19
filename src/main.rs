7aa66d79afa1aeee2b93e040e72d0ff3e8b90c39
shahmeerwsatti <shahmeersatti183@gmail.com>
2024-06-19 14:38:22 +0500

    Made a guessing game from the Rust Book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main()
+{
+println!("Guess the number: ");
+
+let mut guessednum = String::new();
+
+let secretnum: u32 = rand::thread_rng().gen_range(1..=100_);
+
+io::stdin().read_line(&mut guessednum).expect("Failed to read line.");
+
+println!("You guessed: {guessednum}");
+
+let guessednum: u32 = guessednum.trim().parse().expect("Please type a number!");
+
+match guessednum.cmp(&secretnum)
+{
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too big!"),
+    Ordering::Equal => println!("You win!"),
+}
 }
